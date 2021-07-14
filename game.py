b8c3b884aea74900a7764f8cfba2dab5a8fb2bff
Rajath Subramanyam <rajathsiyer@gmail.com>
2021-07-13 20:58:56 -0400

    finished the game

--- game.py
+++ game.py
@@ -7,10 +7,16 @@ pygame.init()
 BLACK = (0, 0, 0)
 WIDTH = 1280
 HEIGHT = 720
+FONT= pygame.font.SysFont("comicsans", 100)
 
 NUM_TO_IMAGE = {0: pygame.image.load('standing.png'), 1: pygame.image.load('moving.png')}
-
-
+LETTER_TO_PYGAME = {'a': pygame.K_a, 'b': pygame.K_b, 'c': pygame.K_c, 'd': pygame.K_d, 
+'e': pygame.K_e, 'f': pygame.K_f, 'g': pygame.K_g, 'h': pygame.K_h, 'i': pygame.K_i, 
+'j': pygame.K_j, 'k': pygame.K_k, 'l': pygame.K_l, 'm': pygame.K_m, 'n': pygame.K_n, 
+'o': pygame.K_o, 'p': pygame.K_p, 'q': pygame.K_q, 'r': pygame.K_r, 's': pygame.K_s, 
+'t': pygame.K_t, 'u': pygame.K_u, 'v': pygame.K_v, 'w': pygame.K_w, 'x': pygame.K_x, 
+'y': pygame.K_y, 'z': pygame.K_z}
+ALPHA_LST = list('abcdefghijklmnopqrstuvwxyz')
 class Stickman:
     def __init__(self, player):
         """
@@ -41,7 +47,6 @@ class Stickman:
             s.blit(NUM_TO_IMAGE[0], self.position)
             self.curr_image = 0
 
-
 class Game:
     def __init__(self, game_id):
         self.p0 = Stickman(0)
@@ -58,9 +63,13 @@ class Game:
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         clock = pygame.time.Clock()
         run = True
+        random_index = random.randint(0,25)
+        temp_Key = LETTER_TO_PYGAME[ALPHA_LST[random_index]]
         while run:
-            clock.tick(60)
-
+            clock.tick(10)
+           
+           
+           
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -68,61 +77,27 @@ class Game:
 
                 if event.type == pygame.K_ESCAPE:
                     run = False
+        
+                if event.type == pygame.KEYDOWN:
+                    if event.key == temp_Key:
+                        random_index = random.randint(0,25)
+                        temp_Key = LETTER_TO_PYGAME[ALPHA_LST[random_index]]
 
-            self.p0.move()
+                        self.p0.move()
             self.p1.move()
 
-            self.p0.curr_image = int(not self.p0.curr_image)
-            self.p1.curr_image = int(not self.p1.curr_image)
-
             screen.fill((214, 214, 214))
+            text = FONT.render(ALPHA_LST[random_index], 1, (0,0,0))
+            screen.blit(text, (50 , 50))
+
             pygame.draw.line(screen, BLACK, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), 4)  # vertical line
             self.p0.draw(screen)
             self.p1.draw(screen)
             pygame.display.update()
 
 
-"""
-def main():  # can remove
-    # game.p0 = Stickman()
-    # game.p1 = Opponent()
-    game = Game(0)
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-    pygame.display.set_caption("Stick Adventures")
-    looping = True
-
-    while looping:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                looping = False
-                pygame.quit()
-
-        screen.fill((214, 214, 214))
-        pygame.draw.line(screen, BLACK, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 4)
-
-        game.p0.move()
-        game.p1.move()
-
-        if game.p0.curr_image == 0:
-            screen.blit(NUM_TO_IMAGE[1], game.p0.position)
-            game.p0.curr_image = 1
-        else:
-            screen.blit(NUM_TO_IMAGE[0], game.p0.position)
-            game.p0.curr_image = 0
-
-        if game.p1.curr_image == 0:
-            screen.blit(NUM_TO_IMAGE[1], game.p1.position)
-            game.p1.curr_image = 1
-        else:
-            screen.blit(NUM_TO_IMAGE[0], game.p1.position)
-            game.p1.curr_image = 0
-
-        pygame.display.update()
-        clock.tick(10)
-"""
 
 
-# if __name__ == '__main__':
-#     game_ = Game(0)
-#    game_.run()
+if __name__ == '__main__':
+    game_ = Game(0)
+    game_.run()
